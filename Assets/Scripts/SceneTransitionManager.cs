b8a9dce49a294b52a19cc959928fa005f9dbfd11
Oliver Calman <olivercalman@Olivers-MacBook-Air-2.local>
2024-10-26 20:36:19 +1100

    added HUD and exit button

--- Assets/Scripts/SceneTransitionManager.cs
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -3,19 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartSceneManager : MonoBehaviour
+public class SceneTransitionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LoadLevel(string scene)
     {
         SceneManager.LoadScene(scene);
