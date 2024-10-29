a8d51af5524de7df1ee0c4dd2b8ac5cb7d2a0a57
Oliver Calman <olivercalman@Olivers-MacBook-Air-2.local>
2024-10-29 20:47:35 +1100

    completed player movement

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -41,16 +41,11 @@ public class PacStudentController : MonoBehaviour
                 //pass input and attempt movement
                MovementHandler(lastInput);
             }
-            /*
-            Check lastInput and try to move in that direction to the
-            adjacent grid position.
-            If the adjacent grid position from lastInput is walkable, then
-            store lastInput in a member variable called “currentInput” 
-            */
     }
 
     public void GetInput()
     {
+        //get current keycode and assign it to lastinput
             if(Input.GetKeyDown(KeyCode.W))
             {
                 lastInput = KeyCode.W;
@@ -71,69 +66,55 @@ public class PacStudentController : MonoBehaviour
 
     public void MovementHandler(KeyCode input)
     {
-        Debug.Log("last Input = " + lastInput);
-        Debug.Log("Current input = " + currentInput);
-
-        InputHandler(lastInput);
-
-        if (CollisionDetector(movementTarget) == false)
+        //if lastinput is a valid movement, move and assign to current input. 
+        if (CollisionDetector(lastInput) == false)
         {
             currentInput = lastInput;
+
             tweener.AddTween(pacStudent.transform, pacStudent.transform.position, movementTarget, 0.5f);  
+            
+            //AnimationHandler(lastInput);
             animator.Play(animDirection);
         }
-        else if (CollisionDetector(movementTarget) == true)
+        else if (CollisionDetector(currentInput) == false) //if currentmovement is valid then move once able
         {
-            
-            //if currentinput equals a tile in tilemap/collision from collisionDetector
-            //then stop movement
-            //else move to the next tile
-            //this should have the effect of allowing direction changes only when the direction can be changed
-            //i.e. hold down key and once wall disappears then pacstudent will move 
-            //ONLY if the previous lerp has finished, so PacStudent is in middle off map
-            //Check distance to movementtarget is close to 0.0 to determine lerp is done
-
-            //move last input. Store next input as currentInput until no collision is recorded and lerp is done, then clear current input.
+            tweener.AddTween(pacStudent.transform, pacStudent.transform.position, movementTarget, 0.5f);  
+            //AnimationHandler(currentInput);
+            animator.Play(animDirection);
         }
+        //unhandled condition results in stopping when wall is hit
                                 
     }
-
-    public bool CollisionDetector(Vector3 movementTarget)
+    public bool CollisionDetector(KeyCode input)
     {
+        //get movement target based on input and assign correct animation to be used in movementhandler
+        switch(input)
+        {
+            case KeyCode.W:
+                movementTarget = pacStudent.transform.position + Vector3.up;
+                animDirection = "Up";    
+                break;
+            case KeyCode.A:
+                movementTarget = pacStudent.transform.position + Vector3.left;
+                animDirection = "Left";  
+                break;
+            case KeyCode.S:
+                movementTarget = pacStudent.transform.position + Vector3.down;
+                animDirection = "Down";  
+                break;
+            case KeyCode.D:
+                movementTarget = pacStudent.transform.position + Vector3.right;
+                animDirection = "Right";  
+                break;
+        }
         //if colliding with tile return true
         if (tilemap.GetTile<Tile>(tilemap.WorldToCell(movementTarget)) != null)
         {
-            Debug.Log("collision");
             return true;
         }
         else
         {
             return false;
         }
-    }
-    private void InputHandler(KeyCode input)
-    {
-       // if (input != KeyCode.None)
-       // {
-            switch(input)
-            {
-                case KeyCode.W:
-                    movementTarget = pacStudent.transform.position + Vector3.up;
-                    animDirection = "Up";
-                    break;
-                case KeyCode.A:
-                    movementTarget = pacStudent.transform.position + Vector3.left;
-                    animDirection = "Left";
-                    break;
-                case KeyCode.S:
-                    movementTarget = pacStudent.transform.position + Vector3.down;
-                    animDirection = "Down";
-                    break;
-                case KeyCode.D:
-                    movementTarget = pacStudent.transform.position + Vector3.right;
-                    animDirection = "Right";
-                    break;
-            }
-       // }
     }
 } 
