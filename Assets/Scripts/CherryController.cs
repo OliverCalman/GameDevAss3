3725f3bc5a0f9ad84108338d534ead67ce47c509
Oliver Calman <olivercalman@Olivers-MacBook-Air-2.local>
2024-10-30 23:19:41 +1100

    generate random position for cherry prefab

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 
 public class CherryController : MonoBehaviour
 {
-    [SerializeField] private GameObject cherry;
+    public GameObject cherry;
     private Tweener tweener;
-    private Animator animator;
     private Vector3 startPosition;
     private Vector3 movementTarget;
     // Start is called before the first frame update
     void Start()
     {
         //just in case, destroy the existing cherry
-      //  Destroy(cherry);
         tweener = GetComponent<Tweener>();
         StartCoroutine(CherryTimer());
     }
@@ -28,36 +26,68 @@ public class CherryController : MonoBehaviour
     {
         while (true)
         {
-            CreateCherry();
-            yield return new WaitForSeconds(10f);
+            yield return new WaitForSeconds(2f);
+            DoCherry();
         }
+
     }
-    private void CreateCherry()
+    private void DoCherry()
     {
-        //if cherry doesn't exist then create it and set random start position
-        Instantiate(cherry);
+        //generate a random value on a range based on max x and y values. Maybe use the camera size
+        //randomly pick whether x or y range will be used
+        //depending on range used, select a side using a fixed value
+        //i.e. if y is used, set x as either -14 or 32
 
-        float x = Random.Range(-14f, 32f);
-        float y = Random.Range(10f, 26f);
+        float x = Random.Range(-22f, 40f);
+        float y = Random.Range(-26f, 10f);
 
-        startPosition = new Vector3(x, y, 0);
-        cherry.transform.position = startPosition;
+        int a = Random.Range(0,2); 
+        if (a == 0)
+        {
+            int b = Random.Range(0,2);
+            if (b == 0)
+            {
+                startPosition = new Vector3(x, -26f, 0);
+            }
+            else if (b == 1)
+            {
+                startPosition = new Vector3(x, 10f, 0);
+            }
+        }
+        else if (a == 1)
+        {
+            int b = Random.Range(0,2);
+            if (b == 0)
+            {
+                startPosition = new Vector3(-22f, y, 0);
+            }
+            else if (b == 1)
+            {
+                startPosition = new Vector3(40f, y, 0);
+            }
+        }
+
+        //get distance d of range from lowest value
+        //set point on opposing range by subtracting distance d from max in range
+        //use function to select side above to determine which side of screen inverse range is on
+
+        //create cherry and set random start position
+        Instantiate(cherry, startPosition, Quaternion.identity);
 
         //set movementtarget on opposite side of screen
         float xInv = 32 - x;
         float yInv = 26 - y;
         movementTarget = new Vector3(0, -5 ,0);
 
-        //upper left bound is (-14,10,0)
-        //lower left bound is (-14,26,0)
-        //upper right bound is (32,10.0)
-        //lower right bound is (32,26,0)
-
         //animate cherry
         tweener.AddTween(cherry.transform, startPosition, movementTarget, 5f);  
-
+        Debug.Log(tweener.activeTween);
         //destroy cherry
-       // Destroy(cherry);
+        if (tweener.activeTween == null)
+        {
+            //destroy cherry once it finishes moving
+            Destroy(cherry);
+        }
 
     }
 }
